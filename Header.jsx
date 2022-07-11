dc388f6028360b4ba3efba3fe0a33d93b822d832
mikeflintrop <101838913+mikeflintrop@users.noreply.github.com>
2022-07-11 16:48:40 -0500

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+function Header () {
+
+    // return one parent, please
+    // <> fragment </>
+    return (
+        <div>
+            <h1>Hello World!!</h1>
+            <h3>Let's React!</h3>
+        </div>
+    );
+}
+
+export default Header;
